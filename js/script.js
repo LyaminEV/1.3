5f655a4b8fa81b2626ea7efee3458266c8876d14
LyaminEV <Daim20212@yandex.ru>
2020-12-10 22:11:54 +0300

    brands

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-new Swiper ('.brands__container', {
+let mySwiper = new Swiper('.brands__container', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -6,4 +6,12 @@ new Swiper ('.brands__container', {
     spaceBetween: 16,
     loop: true,
     slidesPerView: 'auto',
+    breakpoints: {
+        768: {
+            
+            // mySwiper.destroy(
+            //     deleteInstance: false,
+            // );
+        }
+    }
 });
