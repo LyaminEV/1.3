5f9297d4ffd7de196d9994466ba1c039a708312a
LyaminEV <Daim20212@yandex.ru>
2020-12-10 20:48:45 +0300

    brands 320 done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-new Swiper ('.brands-container', {
+new Swiper ('.brands__container', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
+    spaceBetween: 16,
+    loop: true,
+    slidesPerView: 'auto',
 });
