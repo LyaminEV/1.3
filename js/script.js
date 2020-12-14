77da5ee1c64a8ee6d2530fcd501eeffad5c53aac
LyaminEV <Daim20212@yandex.ru>
2020-12-14 14:47:45 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
-let mySwiper = new Swiper('.brands__container', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    spaceBetween: 16,
-    loop: true,
-    slidesPerView: 'auto',
-    breakpoints: {
-        768: {
-            
-            // mySwiper.destroy(
-            //     deleteInstance: false,
-            // );
-        }
+if (screen.width < 768) {
+    let mySwiper = new Swiper('.brands__container', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        spaceBetween: 16,
+        loop: true,
+        slidesPerView: 'auto',
+    });
+}
+let brandList = document.querySelector('.brands__list');
+let button = document.querySelector('.brands__button');
+let buttonText = button.textContent = 'Показать все';
+button.addEventListener('click', function () {
+    if (buttonText  === 'Показать все') {
+        brandList.classList.add('brands__list--open');
+        button.textContent = 'Скрыть';
+    } else {
+        brandList.classList.remove('brands__list--open');
+        
     }
 });
+
